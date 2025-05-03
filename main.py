96158a3f0a335afc4267ef1fc73a32815bfcf695
yaminoryu <lordwiard@gmail.com>
2025-05-03 06:43:24 -0400

    created player.py, circleshape.py, and updated constants.py. added player to main and added rotation to player

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import pygame
 from constants import *
+from player import Player
+
 
 black = (0,0,0)
+
+
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(black)
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
-
-
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
---
+++ player.py
@@ -0,0 +1,36 @@
+from constants import *
+from circleshape import *
+import pygame
+
+
+white = (255,255,255)
+
+class Player(CircleShape):
+        def __init__(self, x, y):
+            super().__init__(x, y, PLAYER_RADIUS)
+
+            self.rotation = 0
+
+
+        # Shape of player.
+        def triangle(self):
+            forward = pygame.Vector2(0, 1).rotate(self.rotation)
+            right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+            a = self.position + forward * self.radius
+            b = self.position - forward * self.radius - right
+            c = self.position - forward * self.radius + right
+            return [a, b, c]
+
+        def draw(self, screen):
+            pygame.draw.polygon(screen, white, self.triangle())
+
+        def rotate(self, dt):
+            self.rotation += PLAYER_TURN_SPEED * dt
+
+        def update(self, dt):
+            keys = pygame.key.get_pressed()
+
+            if keys[pygame.K_a]:
+                self.rotate(-dt)  # Rotate left by passing negative dt
+            if keys[pygame.K_d]:
+                self.rotate(dt)   # Rotate right by passing positive dt
